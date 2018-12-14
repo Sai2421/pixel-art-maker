211d7759dd1eb151943cc609f17a4e8873f716b1
Saisaran mummidi <37036297+Sai2421@users.noreply.github.com>
2018-12-14 21:15:05 +0530

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,45 @@
+
+
+
+    var a, b;
+   
+    
+    $(document).ready(function () {
+
+        $(document).on('click','.ett',function(){
+            
+            var co =$('#colorPicker').val();
+               $(this).css('background-color',co);
+            })
+            
+            $(document).on('click','#fill',function(){
+                var c =$('#colorPicker').val();
+                $('table').css('background-color',c);
+            })
+
+        $('#sam').click(function () {
+            a = $('#inputHeight').val();
+            b = $('#inputWidth').val();
+        
+            for (let i = 0; i < a; i++) {
+                $('table').append('<tr class="row"></tr>');
+                for (let j = 0; j < b; j++)
+                {
+                    $('tr').filter(':last').append('<td class="ett"></td>');
+                }
+                
+            }
+        })
+        
+        
+    })
+    
+
+        
+    
+    
+
+    
+   
+
+
